aac95184b87915e74b86dad98d5b5bc314025c93
Joshua Yeung <joshua.chyeung@gmail.com>
2020-09-12 18:09:20 +0800

    Complete Day 2 Homework

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,26 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-function Product({ title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  console.log("this is the basket >>> ", basket);
+
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -14,13 +33,16 @@ function Product({ title, image, price, rating }) {
           {Array(rating)
             .fill()
             .map((_, i) => (
-              <p>🌟</p>
+              <span role="img" aria-label="star">
+                🌟
+              </span>
             ))}
         </div>
       </div>
 
       <img src={image} alt="" />
-      <button>Add to Basket</button>
+
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
---
+++ src/Subtotal.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format";
+import { useStateValue } from "./StateProvider";
+
+function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
+
+  return (
+    <div className="subtotal">
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              {/* Part of the homework */}
+              Subtotal ({basket?.length} items):
+              <strong>{value}</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" /> This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={basket?.reduce((amount, item) => item.price + amount, 0)} // part of the homework
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+      <button>Proceed to Checkout</button>
+    </div>
+  );
+}
+
+export default Subtotal;
