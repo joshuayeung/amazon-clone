629ef2955c0c2c8a7e2ea3dbd545c2ab87d9e222
Joshua Yeung <joshua.chyeung@gmail.com>
2020-09-12 14:42:44 +0800

    day 2

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
@@ -11,7 +12,7 @@ function Header() {
       />
 
       <div className="header__search">
-        <input className="header___searchInput" type="text" />
+        <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon" />
       </div>
 
@@ -20,15 +21,17 @@ function Header() {
           <span className="header__optionLineOne">Hello Guest</span>
           <span className="header__optionLineTwo">Sign in</span>
         </div>
-
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
           <span className="header__optionLineTwo">& Orders</span>
         </div>
-
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
