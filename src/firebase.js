9d3f3617ff3f368c242e776529a25096e27c115b
Joshua Yeung <joshua.chyeung@gmail.com>
2020-09-12 12:13:18 +0800

    init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBPgmEp8Ncv6zdnxTxVhPbbfvdvSkgSJEA",
+  authDomain: "challenge-d9467.firebaseapp.com",
+  databaseURL: "https://challenge-d9467.firebaseio.com",
+  projectId: "challenge-d9467",
+  storageBucket: "challenge-d9467.appspot.com",
+  messagingSenderId: "1076357587428",
+  appId: "1:1076357587428:web:5dc75adb9bbc16488c67f1",
+  measurementId: "G-38BCZC4298"
+};
