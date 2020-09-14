5f93b4dabe61ff11fba9831f1610845b312de6b0
Joshua Yeung <joshua.chyeung@gmail.com>
2020-09-14 12:59:57 +0800

    Day 4 payment with stripe

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51HQshAEFNXiDs2MYtqJZCIO6Kj1UrtOsLraygOmEoivhh91W8URGO2nxkjRJ2ftcv892Qez1NSTzuhAIA1xHO4wf00HK35ac5I"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -42,6 +49,12 @@ function App() {
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
